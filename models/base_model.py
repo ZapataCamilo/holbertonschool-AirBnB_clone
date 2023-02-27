fe6bd56a5cd46ae378bf6ea2f69fd6dae6483fca
Dannyelgeek <daot1993@gmail.com>
2023-02-27 15:51:07 -0500

    Python: AirBnB console task 3

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''Python AirBnB Console Project'''
+import uuid
+import datetime
+import models
+
+
+class BaseModel():
+    '''Defines all common attributes/methods for other classes'''
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    
+    def __str__(self):
+        '''Print a string with Class data'''
+        name = type(self).__name__
+        dict = self.__dict__
+        return f"[{name}] ({self.id}) {dict}"
+
+    def save(self):
+        '''Updates the public instance attribute'''
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        ''' returns a dictionary containing all\
+         keys/values of __dict__ of the instance'''
+        dict_cpy = self.__dict__.copy()
+        dict_cpy["created_add"] = self.created_at.isoformat()
+        dict_cpy["updated_add"] = self.updated_at.isoformat()
+        dict_cpy["__class__"] = type(self).__name__
+        return dict_cpy
