798cf31aaea07306a357a92b288817962add2ae5
Dannyelgeek <daot1993@gmail.com>
2023-02-27 16:15:28 -0500

    Python: AirBnB console task 3

--- models/base_model.py
+++ models/base_model.py
@@ -7,9 +7,9 @@ import models
 
 class BaseModel():
     '''Defines all common attributes/methods for other classes'''
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime()
+        self.created_at = datetime.time()
         self.updated_at = datetime.time()
     
     def __str__(self):
@@ -20,7 +20,7 @@ class BaseModel():
 
     def save(self):
         '''Updates the public instance attribute'''
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.time()
 
     def to_dict(self):
         ''' returns a dictionary containing all\
