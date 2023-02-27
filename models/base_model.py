685ce235b10b868a3e1809cce8496b10d5c38fa0
Dannyelgeek <daot1993@gmail.com>
2023-02-27 16:07:47 -0500

    Python: AirBnB console task 3

--- models/base_model.py
+++ models/base_model.py
@@ -9,8 +9,8 @@ class BaseModel():
     '''Defines all common attributes/methods for other classes'''
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.time()
-        self.updated_at = datetime.time()
+        self.created_at = datetime.datetime()
+        self.updated_at = datetime.datetime()
     
     def __str__(self):
         '''Print a string with Class data'''
