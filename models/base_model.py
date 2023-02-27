86be1b2e5f202209cd50c403ca086c0541a8218a
Dannyelgeek <daot1993@gmail.com>
2023-02-27 16:49:35 -0500

    Python: AirBnB console task 3

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,12 @@ import datetime
 
 class BaseModel():
     '''Defines all common attributes/methods for other classes'''
+    date = datetime.datetime(year=2023, month=2, day=27, hour=16, minute=26, tzinfo=None)
+
     def __init__(self, *args):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime(year=2023, month=2, day=27, hour=16, minute=26, tzinfo=None)
-        self.updated_at = datetime.datetime(year=2023, month=2, day=27, hour=16, minute=26, tzinfo=None)
-
+        self.created_at = BaseModel.date
+        self.updated_at = BaseModel.date
     def __str__(self):
         '''Print a string with Class data'''
         name = type(self).__name__
@@ -19,8 +20,7 @@ class BaseModel():
 
     def save(self):
         '''Updates the public instance attribute'''
-        self.updated_at = datetime.datetime(year=2023, month=2, day=27, hour=16, minute=26, tzinfo=None)
-
+        self.updated_at = BaseModel.date
     def to_dict(self):
         ''' returns a dictionary containing all\
          keys/values of __dict__ of the instance'''
