ef42c45ea56f2bc07dcbc1d9872fa3837321ae0f
Dannyelgeek <daot1993@gmail.com>
2023-02-28 13:31:08 -0500

    Python: AirBnB console test 3

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,5 +14,10 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(datetime.datetime, type(test_base.created_at))
         self.assertEqual(datetime.datetime, type(test_base.updated_at))
 
+    def test_str(self):
+        '''Testing __str__ method'''
+        test_base = BaseModel()
+        self.assertEqual(type(str(test_base)), str)
+
 if __name__ == "__main__":
     unittest.main()
