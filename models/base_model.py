5654fca1e72dda6fdc45e13dcaef943918c93780
Dannyelgeek <daot1993@gmail.com>
2023-02-28 06:32:11 -0500

    Python: AirBnB console task 3

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel():
 
     def __str__(self):
         '''Print a string with Class data'''
-        name = __name__
+        name = type(self).__name__
         dict = self.__dict__
         return f"[{name}] ({self.id}) {dict}"
 
