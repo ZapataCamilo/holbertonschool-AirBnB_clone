7eb2cc3ead4dc1ba691707ed68ef1ca82fe11c1a
Dannyelgeek <daot1993@gmail.com>
2023-02-28 07:59:40 -0500

    Python: AirBnB console task 4

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         if kwargs is not None:
+            self.id = str(uuid.uuid4())
             self.created_at = str(BaseModel.date)
             self.updated_at = str(BaseModel.date)
         else:
