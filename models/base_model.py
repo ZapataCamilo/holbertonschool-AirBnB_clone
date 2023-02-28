5fcadea2dcd3accb8c83a3f57ec44e9aadc62e6a
Dannyelgeek <daot1993@gmail.com>
2023-02-28 18:01:14 -0500

    Python: AirBnB console task 4

--- models/base_model.py
+++ models/base_model.py
@@ -6,16 +6,20 @@ from datetime import datetime
 
 class BaseModel():
     '''Defines all common attributes/methods for other classes'''
-    date = datetime.now()
 
     def __init__(self, *args, **kwargs):
-        if kwargs is not None:
-            self.id = str(uuid.uuid1())
-            self.created_at = BaseModel.date
-            self.updated_at = BaseModel.date
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == 'id':
+                    self.id = v
+                if k == 'created_at':
+                    self.created_at = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k == 'updated_at':
+                    self.updated_at = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = BaseModel.date
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''Print a string with Class data'''
@@ -25,13 +29,13 @@ class BaseModel():
 
     def save(self):
         '''Updates the public instance attribute'''
-        self.updated_at = BaseModel.date
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         ''' returns a dictionary containing all\
          keys/values of __dict__ of the instance'''
-        return {"id": str(uuid.uuid4()),
-                "created_at": BaseModel.date.isoformat(),
-                "updated_at": BaseModel.date.isoformat(),
+        return {"id": self.id,
+                "created_at": self.created_at.isoformat(),
+                "updated_at": self.updated_at.isoformat(),
                 "__class__": type(self).__name__
                 }
