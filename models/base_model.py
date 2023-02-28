1f80be45bcf4d31e7eea87dfe93d6982f74979c6
Dannyelgeek <daot1993@gmail.com>
2023-02-28 07:50:56 -0500

    Python: AirBnB console task 4

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,9 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         if kwargs:
+            self.id = str(uuid.uuid4())
             self.created_at = str(BaseModel.date)
             self.updated_at = str(BaseModel.date)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = BaseModel.date
 
     def __str__(self):
         '''Print a string with Class data'''
