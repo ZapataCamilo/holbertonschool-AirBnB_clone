7e73fb13baac384c8654e9fc6df6f108364d29be
Dannyelgeek <daot1993@gmail.com>
2023-03-01 06:45:24 -0500

    Python: AirBnB console task 4

--- models/base_model.py
+++ models/base_model.py
@@ -13,9 +13,11 @@ class BaseModel():
                 if k == 'id':
                     self.id = v
                 if k == 'created_at':
-                    self.created_at = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.created_at = datetime.strptime(v,
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
                 if k == 'updated_at':
-                    self.updated_at = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.updated_at = datetime.strptime(v, 
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
