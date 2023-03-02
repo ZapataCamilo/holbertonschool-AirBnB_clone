45fdf6241995b4284285a1a8dedcce0d1051c40f
Dannyelgeek <daot1993@gmail.com>
2023-03-02 06:52:01 -0500

    Python: AirBnB console task 5

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ class BaseModel():
     '''Defines all common attributes/methods for other classes'''
 
     def __init__(self, *args, **kwargs):
-        from models import storage
+        '''from models import storage'''
         if kwargs:
             for k, v in kwargs.items():
                 if k == 'id':
@@ -20,7 +20,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            '''storage.new(self)'''
 
     def __str__(self):
         '''Print a string with Class data'''
@@ -29,10 +29,10 @@ class BaseModel():
         return f"[{name}] ({self.id}) {dict}"
 
     def save(self):
-        from models import storage
         '''Updates the public instance attribute'''
+        '''from models import storage'''
         self.updated_at = datetime.now()
-        storage.save()
+        '''storage.save()'''
 
     def to_dict(self):
         '''returns a dictionary containing all\
