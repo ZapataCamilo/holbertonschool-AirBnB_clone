129d3649a144ccfaacb654eaf45b5ed5c8bfe38d
ZapataCamilo <zapatacamilo86@gmail.com>
2023-03-03 10:46:49 -0500

    Python: AirBnB console task 9

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Python AirBnB Console Project'''
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    '''inherits from BaseModel'''
+    state_id = ''
+    name = ''
