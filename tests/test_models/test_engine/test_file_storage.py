eda5cc49178299b756f5834127abe684d41be25d
Dannyelgeek <daot1993@gmail.com>
2023-03-03 10:12:02 -0500

    Python: AirBnB console test 5

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,6 +4,7 @@ import unittest
 import json
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.user import User
 from models import storage
 all_data = storage.all()
 
@@ -16,6 +17,19 @@ class TestFileStorage(unittest.TestCase):
         self.assertIsNotNone(all_data)
         self.assertEqual(dict, type(all_data))
         self.assertIs(all_data, storage._FileStorage__objects)
+
+    def test_new(self):
+        '''Testing new mwthod'''
+        storage = FileStorage()
+        saitama = User()
+        saitama.id = 75342
+        saitama.first_name = 'Saitama'
+        saitama.last_name = 'calvo'
+        storage.new(saitama)
+        k = saitama.__class__.__name__ + '.' + str(saitama.id)
+        self.assertIsNotNone(all_data[k])
+
+    
 
 if __name__ == "__main__":
     unittest.main()
