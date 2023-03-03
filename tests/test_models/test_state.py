5a8792fce7b570a1255b1b903100801eb8cba0fc
Dannyelgeek <daot1993@gmail.com>
2023-03-03 12:27:55 -0500

    Python: AirBnB console test 9

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Testing user.py'''
+import unittest
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    '''Testing State'''
+    def test_name(self):
+        '''Testing state attribute'''
+        texas = State()
+        self.assertEqual(str, type(texas.name))
+        self.assertIsNotNone(texas.name)
+
+if __name__ == "__main__":
+    unittest.main()
